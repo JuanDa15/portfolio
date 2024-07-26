d2e4542c3503c80b48248dec3a1be8dfa9f3f97d
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-07-25 23:31:23 -0500

    end home landing styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
     extend: {
       colors: {
-        'neon-blue': '#1E90FF',
-        'sky-blue': '#87CEEB',
-        'dodger-blue': '#1E90FF',
-        cyan: '#00FFFF',
-        'deep-sky-blue': '#00BFFF',
-        'light-blue': '#ADD8E6',
         'electric-purple': '#9B30FF',
         lavender: '#E6E6FA',
         'deep-purple': '#6A0DAD',
