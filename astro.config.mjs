7bbe987ccfe634aa7142d3d010151531d7bafa9e
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-30 21:46:03 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio/',
-  site: 'https://juanda15.github.io',
+  base: '',
+  site: 'https://juanda15.github.io/portfolio/',
   integrations: [tailwind()],
   output: 'static',
   i18n: {
