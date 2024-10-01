fdcc42ce40e095bd16b089e19bc4198cd9a8c6c1
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-30 20:59:44 -0500

    .

--- src/services/experience.ts
+++ src/services/experience.ts
@@ -1,7 +1,7 @@
 import type { IExperience } from "../types"
 
 export async function getAllExperience(locale: string | undefined = 'en') {
-  const resp = await fetch(`https://github.com/JuanDa15/portfolio/public/data/experience.json`)
+  const resp = await fetch(`https://github.com/JuanDa15/portfolio/data/experience.json`)
   const data = await resp.json()
   return data[locale] as IExperience[]
 }
