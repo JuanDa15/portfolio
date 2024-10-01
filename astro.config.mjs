af0bb3695239b16a68d8cbee68b3cd791ba17042
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-30 21:12:01 -0500

    add base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base: 'https://github.com/JuanDa15/portfolio',
   integrations: [tailwind()],
   output: 'static',
   i18n: {
