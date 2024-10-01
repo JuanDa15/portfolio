c1854311967c26fcc3c7cb7ddb127dcc3f7fb302
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-30 20:54:31 -0500

    fix  fetch route

--- src/services/experience.ts
+++ src/services/experience.ts
@@ -1,7 +1,7 @@
 import type { IExperience } from "../types"
 
 export async function getAllExperience(locale: string | undefined = 'en') {
-  const resp = await fetch(`http://localhost:4321/data/experience.json`)
+  const resp = await fetch(`/data/experience.json`)
   const data = await resp.json()
   return data[locale] as IExperience[]
 }
